9dc5f09627ee470a2aced98c91b14ee933d7ae8f
Your Name <youremail@yourdomain.com>
2021-08-04 16:18:52 -0400

    created a map for business l;ocation. It gis a response but doesn't show in modal pop up

--- client/app.js
+++ client/app.js
@@ -41,48 +41,63 @@ function displayYelp(businesses) {
       <div class='display-box' style='height:600px; width:500px;'>
       <h1 id="background-text" class='city'>${business.location.city}</h1>
       <h1 id="background-text" class='business-name'>${business.name}</h1>
+	
       </div>
+      
      `;
+     bizDiv.insertAdjacentHTML("beforeend", display);
+     bizDiv.addEventListener("click", () => {
+       // let modalContent = document.querySelector(".modal-content");
+       // modalContent.style.backgroundImage = `url(${business.image_url})`;
+ 
+       document.querySelector(".modal-text").innerHTML = `
+           <h1 class="restaurant-name">${business.name}</h1>
+           <h3>${business.categories[0].title}</h3>
+           <h2>${business.price}</h2>
+           <p>${business.location.display_address[0]}</p>
+           <p>${business.location.display_address[1]}</p>
+           <p>${business.location.display_address[2]}</p>
+           <h6>Phone</>
+           <p>${business.phone}</p>
+           <img class="logo-4modal" src="https://i.imgur.com/6mH212q.png"/>
+           `;
+         // createMap(`${business.coordinates.latitude}`, `${business.coordinates.longitude}`)
+         console.log(`${business.coordinates.latitude}`, `${business.coordinates.longitude}`)
+         document.querySelector(".modal").style.display = "block";
+     });
+   
+     window.onclick = function (event) {
+       if (event.target == document.querySelector(".modal")) {
+         document.querySelector(".modal").style.display = "none";
+       }
+     };
+     let span = document.querySelector(".close");
+     let modal = document.querySelector(".modal");
+     span.onclick = function () {
+       modal.style.display = "none";
+     };
+   });
+ }
+ 
+ 
+ // function createMap(long, lat) {
+ //   mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ';
+ //             let map = new mapboxgl.Map({
+ //             container: 'map',
+ //             style: 'mapbox://styles/mapbox/streets-v11',
+ //             center: [long, lat],
+ //             zoom: 8
+ //             });
+           
+ // }
+ function removeBusinesses() {
+   const removeElement = document.querySelector(".biz-data");
+   while (removeElement.lastChild) {
+     removeElement.removeChild(removeElement.lastChild);
+   }
+ }
 
-    bizDiv.insertAdjacentHTML("beforeend", display);
-    bizDiv.addEventListener("click", () => {
-      // let modalContent = document.querySelector(".modal-content");
-      // modalContent.style.backgroundImage = `url(${business.image_url})`;
-
-      document.querySelector(".modal-text").innerHTML = `
-          <h1 class="restaurant-name">${business.name}</h1>
-          <h3>${business.categories[0].title}</h3>
-          <h2>${business.price}</h2>
-          <p>${business.location.display_address[0]}</p>
-          <p>${business.location.display_address[1]}</p>
-          <p>${business.location.display_address[2]}</p>
-          <h6>Phone</>
-          <p>${business.phone}</p>
-          <p>
-          ${business.coordinates.latitude}
-          ${business.coordinates.longitude}
-          </p>
-          <img class="logo-4modal" src="https://i.imgur.com/6mH212q.png"/>
-        `;
-      document.querySelector(".modal").style.display = "block";
-    });
-
-    window.onclick = function (event) {
-      if (event.target == document.querySelector(".modal")) {
-        document.querySelector(".modal").style.display = "none";
-      }
-    };
-    let span = document.querySelector(".close");
-    let modal = document.querySelector(".modal");
-    span.onclick = function () {
-      modal.style.display = "none";
-    };
-  });
-}
-
-function removeBusinesses() {
-  const removeElement = document.querySelector(".biz-data");
-  while (removeElement.lastChild) {
-    removeElement.removeChild(removeElement.lastChild);
-  }
-}
+    
+    
+    
+    
---
+++ client/map.js
@@ -0,0 +1,10 @@
+// function createMap(long, lat) {
+//   mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ';
+//             let map = new mapboxgl.Map({
+//             container: 'map',
+//             style: 'mapbox://styles/mapbox/streets-v11',
+//             center: [long, lat],
+//             zoom: 8
+//             });
+          
+// }
