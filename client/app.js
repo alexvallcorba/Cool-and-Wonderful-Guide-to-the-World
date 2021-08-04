2f2746b52e4b098770a425180b888b95355dcd21
Your Name <youremail@yourdomain.com>
2021-08-04 18:22:23 -0400

    cleaned up the code. Still working on submit contact form

--- client/app.js
+++ client/app.js
@@ -41,22 +41,21 @@ function displayYelp(businesses) {
       <div class='display-box' style='height:600px; width:500px;'>
       <h1 id="background-text" class='city'>${business.location.city}</h1>
       <h1 id="background-text" class='business-name'>${business.name}</h1>
-	
-      </div>
-      
-     `;
-     bizDiv.insertAdjacentHTML("beforeend", display);
-     bizDiv.addEventListener("click", () => {
-       // let modalContent = document.querySelector(".modal-content");
-       // modalContent.style.backgroundImage = `url(${business.image_url})`;
-       function openForm() {
+	    </div>
+      `;
+
+    bizDiv.insertAdjacentHTML("beforeend", display);
+    bizDiv.addEventListener("click", () => {
+      // let modalContent = document.querySelector(".modal-content");
+      // modalContent.style.backgroundImage = `url(${business.image_url})`;
+      function openForm() {
         document.getElementById("myForm").style.display = "block";
       }
-      
+
       function closeForm() {
         document.getElementById("myForm").style.display = "none";
       }
-       document.querySelector(".modal-text").innerHTML = `
+      document.querySelector(".modal-text").innerHTML = `
            <h1 class="restaurant-name">${business.name}</h1>
            <h3>${business.categories[0].title}</h3>
            <h2>${business.price}</h2>
@@ -66,57 +65,58 @@ function displayYelp(businesses) {
            <h6>Phone</>
            <p>${business.phone}</p>
            <img class="logo-4modal" src="https://i.imgur.com/6mH212q.png"/>
-           <form action="/action_page.php" class="form-container">
-            <h1>Login</h1>
-
-            <label for="email"><b>Email</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required>
-
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-
-            <button type="submit" class="btn">Login</button>
-            <button type="submit" class="btn cancel" onclick="closeForm()">Close</button>
-          </form>
            `;
-        
-         createMap(`${business.coordinates.latitude}`, `${business.coordinates.longitude}`)
-         console.log(`${business.coordinates.latitude}`, `${business.coordinates.longitude}`)
-         document.querySelector(".modal").style.display = "block";
-     });
-   
-     window.onclick = function (event) {
-       if (event.target == document.querySelector(".modal")) {
-         document.querySelector(".modal").style.display = "none";
-       }
-     };
-     let span = document.querySelector(".close");
-     let modal = document.querySelector(".modal");
-     span.onclick = function () {
-       modal.style.display = "none";
-     };
-   });
- }
- 
- 
- function createMap(lat, long) {
-   mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ';
-  let map = new mapboxgl.Map({
-   container: 'map',
-   style: 'mapbox://styles/mapbox/streets-v11',
-   center: [long, lat],
-   zoom: 8
-             });
-           
- }
- function removeBusinesses() {
-   const removeElement = document.querySelector(".biz-data");
-   while (removeElement.lastChild) {
-     removeElement.removeChild(removeElement.lastChild);
-   }
- }
+      document.getElementById("myForm").innerHTML = `
+           <form action="/action_page.php" class="form-container">
+           <h1>Login</h1>
+       
+           <label for="email">Email</label>
+           <input type="text" placeholder="Enter Email" name="email">
+       
+           <label for="psw"><b>Your</b></label>
+           <input type="password" placeholder="Enter Password" name="psw">
+       
+           <button type="submit" class="btn">submit</button>
+           </form>
+        `;
 
-    
-    
-    
-    
+      createMap(
+        `${business.coordinates.latitude}`,
+        `${business.coordinates.longitude}`
+      );
+      console.log(
+        `${business.coordinates.latitude}`,
+        `${business.coordinates.longitude}`
+      );
+      document.querySelector(".modal").style.display = "block";
+    });
+
+    window.onclick = function (event) {
+      if (event.target == document.querySelector(".modal")) {
+        document.querySelector(".modal").style.display = "none";
+      }
+    };
+    let span = document.querySelector(".close");
+    let modal = document.querySelector(".modal");
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  });
+}
+
+function createMap(lat, long) {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ";
+  let map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [long, lat],
+    zoom: 8,
+  });
+}
+function removeBusinesses() {
+  const removeElement = document.querySelector(".biz-data");
+  while (removeElement.lastChild) {
+    removeElement.removeChild(removeElement.lastChild);
+  }
+}
--- client/map.js
+++ client/map.js
@@ -8,3 +8,15 @@
 //             });
           
 // }
+
+{/* <form>
+<div class="form-container">
+ <h1>Sign Up</h1>
+ <input type="text" placeholder="Enter Email" name="email" required>
+ <input type="password" placeholder="Enter Password" name="psw" required>
+ <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+ <div class="button-sign">
+   <button type="submit" class="signupbtn">Sign Up</button>
+ </div>
+ </div>
+ </form> */}
