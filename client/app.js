2af3daf7430898cb5f067cfc6c08d926fbd2bae4
Your Name <youremail@yourdomain.com>
2021-08-05 16:30:29 -0400

    Raul helped me with the map issue. Resolved. Working now on form pop up window.

--- client/app.js
+++ client/app.js
@@ -25,31 +25,29 @@ const getYelpRequest = async (term, location) => {
 };
 
 function displayYelp(businesses) {
-
-    businesses.forEach((business) => {
-      let bizDiv = document.createElement("div");
-      bizDiv.classList = "bizDiv-container";
-      bizDiv.style.backgroundImage = `url(${business.image_url})`;
-      document.querySelector(".biz-data").append(bizDiv);
-      console.log(business.image_url);
-      const display = `
+  businesses.forEach((business) => {
+    let bizDiv = document.createElement("div");
+    bizDiv.classList = "bizDiv-container";
+    bizDiv.style.backgroundImage = `url(${business.image_url})`;
+    document.querySelector(".biz-data").append(bizDiv);
+    console.log(business.image_url);
+    const display = `
       <div class='display-box'>
       <h1 id="background-text" class='city'>${business.location.city}</h1>
       <h1 id="background-text" class='business-name'>${business.name}</h1>
 	    </div>
       `;
 
-      bizDiv.insertAdjacentHTML("beforeend", display);
-      bizDiv.addEventListener("click", () => {
-        
-        function openForm() {
-          document.getElementById("myForm").style.display = "block";
-        }
+    bizDiv.insertAdjacentHTML("beforeend", display);
+    bizDiv.addEventListener("click", () => {
+      function openForm() {
+        document.getElementById("myForm").style.display = "block";
+      }
 
-        function closeForm() {
-          document.getElementById("myForm").style.display = "none";
-        }
-        document.querySelector(".modal-text").innerHTML = `
+      function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+      }
+      document.querySelector(".modal-text").innerHTML = `
            <h1 class="restaurant-name">${business.name}</h1>
            <h3>${business.categories[0].title}</h3>
            <h2>${business.price}</h2>
@@ -58,66 +56,52 @@ function displayYelp(businesses) {
            <p>${business.location.display_address[2]}</p>
            <h6>Phone</>
            <p>${business.phone}</p>
+           <div id="map"></div>
            <img class="logo-4modal" src="https://i.imgur.com/6mH212q.png"/>
            `;
 
-        document.querySelector(".modal").style.display = "block";
-       
-      });
-
-      window.onclick = function (event) {
-        if (event.target == document.querySelector(".modal")) {
-          document.querySelector(".modal").style.display = "none";
-        }
-      };
-      let span = document.querySelector(".close");
-      let modal = document.querySelector(".modal");
-      span.onclick = function () {
-        modal.style.display = "none";
-      };
-    });
-
-    function openForm() {
-      document.getElementById(".form-popup").style.display = "block";
-    }
-
-    function closeForm() {
-      document.getElementById(".form-popup").style.display = "none";
-    }
-    document.getElementById(".form-popup").innerHTML = `
-  <h2>Popup Form</h2>
-  <p>Click on the "Open Form" button to open the popup form.</p>
-  <div class="openBtn">
-    <button class="openButton" onclick="openForm()"><strong>Open Form</strong></button>
-  </div>
-  <div class="loginPopup">
-    <div class="formPopup" id="popupForm">
-      <form action="/action_page.php" class="formContainer">
-        <h2>Please Log in</h2>
-        <label for="email">
-          <strong>E-mail</strong>
-        </label>
-        <input type="text" id="email" placeholder="Your Email" name="email" required>
-        <label for="psw">
-          <strong>Password</strong>
-        </label>
-        <input type="password" id="psw" placeholder="Your Password" name="psw" required>
-        <button type="submit" class="btn">Log in</button>
-        <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
-      </form>
-        `;
-
-    function createMap(lat, long) {
       mapboxgl.accessToken =
         "pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ";
       let map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
-        center: [long, lat],
-        zoom: 8,
+        center: [business.coordinates.longitude, business.coordinates.latitude],
+        zoom: 13,
       });
-    }
-  
+
+      // Create a default Marker and add it to the map.
+      let marker1 = new mapboxgl.Marker()
+        .setLngLat([
+          business.coordinates.longitude,
+          business.coordinates.latitude,
+        ])
+        .addTo(map);
+
+      document.querySelector(".modal").style.display = "block";
+    });
+
+    window.onclick = function (event) {
+      if (event.target == document.querySelector(".modal")) {
+        document.querySelector(".modal").style.display = "none";
+      }
+    };
+    let span = document.querySelector(".close");
+    let modal = document.querySelector(".modal");
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  });
+
+  // function createMap(lat, long) {
+  //   mapboxgl.accessToken =
+  //     "pk.eyJ1IjoiYWxleHZhbGxjb3JiYSIsImEiOiJja3J4dGJ3NDcwdGtjMnBuMWp6Yjh5ZWpsIn0.tlnaGCoqSvhJbp93TvlPaQ";
+  //   let map = new mapboxgl.Map({
+  //     container: "map",
+  //     style: "mapbox://styles/mapbox/streets-v11",
+  //     center: [long, lat],
+  //     zoom: 8,
+  //   });
+  // }
 }
 
 function removeBusinesses() {
@@ -126,3 +110,36 @@ function removeBusinesses() {
     removeElement.removeChild(removeElement.lastChild);
   }
 }
+
+// function popUpForm() {
+
+// document.querySelector("#button-envelop").addEventListener("click", (e) => {
+//   openForm();
+// });
+// function openForm() {
+//   document.getElementClass(".form-popup").style.display = "block";
+// }
+// function closeForm() {
+//   document.getElementClass(".form-popup").style.display = "none";
+// }
+// document.querySelector(".form-popup").innerHTML = `
+//    <h4 class="Contact-Info">Contact Info</h1>
+//    <input class="input3" type="text" id="Name" placeholder="First Name & Last Name"/>
+//    <input class="input4" type="text" id="Name" placeholder="Email address"/>
+//    <button id="button-eyes" type="submit">
+//       <img src="https://i.imgur.com/rZrQr6s.png" alt="eyes" />
+//    </button>
+// `;
+
+// document.querySelector(".form-popup").style.display = "block";
+// window.onclick = function (event) {
+//   if (event.target == document.querySelector(".form-popup")) {
+//     document.querySelector(".form-popup").style.display = "none";
+//   }
+// };
+// let span = document.querySelector(".close");
+// let modal = document.querySelector(".form-popup");
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+// }
